dd1754f4455135ffd693aeee0265dab5ee5009ce
Marco Oddi <marco.oddi@fabersystem.it>
2023-05-23 12:52:51 +0200

    csv

--- app/src/main/java/it/fabersystem/di/rb/pdfextractor/Main.java
+++ app/src/main/java/it/fabersystem/di/rb/pdfextractor/Main.java
@@ -9,6 +9,8 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.FileAppender;
 
+import java.io.IOException;
+
 import org.apache.commons.cli.*;
 
 
@@ -16,7 +18,7 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
  
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         
         CommandLineParser parser = new DefaultParser();
@@ -36,10 +38,22 @@ public class Main {
                 throw new ParseException("Missing mandatory fields");
             }
             */
-            String file = "C:\\Users\\marco.oddi\\Desktop\\pdfbox\\app\\src\\test\\resources\\RBIT04_VATREP_202202_2023_05_08_14_20_12.pdf";
-            String pdf = Process.elabpdfbox(file);
 
-            logger.info("il file contiene: "+pdf);
+
+            // gli passo il file pdf per prendere da esso i dati
+            String filepdf = "C:\\Users\\marco.oddi\\Desktop\\pdfbox\\app\\src\\test\\resources\\RBIT04_VATREP_202202_2023_05_08_14_20_12.pdf";
+            String datiestratti = Process.elabpdfbox(filepdf);
+            logger.info("il file contiene: "+datiestratti);
+
+
+            // gli passo il file csv
+            String filecsv = "C:\\Users\\marco.oddi\\Desktop\\pdfbox\\app\\src\\test\\resources\\RBIT04_VATREP_202202_2023_05_08_14_20_12.csv";
+            try {
+                Process.recuperadati(filecsv,datiestratti);
+            } catch (IOException e) {
+                logger.error("Non è stato possibile accedere al file .csv ", e);
+            }
+
 
         } catch (ParseException e) {
             logger.error("Invalid arguments");
--- app/src/main/java/it/fabersystem/di/rb/pdfextractor/Process.java
+++ app/src/main/java/it/fabersystem/di/rb/pdfextractor/Process.java
@@ -2,18 +2,21 @@ package it.fabersystem.di.rb.pdfextractor;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 public class Process {
     
     public static String elabpdfbox (String PDFfile){
         String dateextract = "";
-        String titolo = "";
-        String Nreport = "";
+
+        //estrae tutto il contenuto della prima pagina
         try {
             File file = new File(PDFfile);
             PDDocument document = PDDocument.load(file);
@@ -29,23 +32,81 @@ public class Process {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
+        try {
+            File file = new File(PDFfile); // Specifica il percorso del tuo file PDF
+            PDDocument document = PDDocument.load(file);
+            
+            PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+            
+            // Specifica le coordinate del rettangolo da cui estrarre il testo
+            float x = 265; // Coordinata x del rettangolo
+            float y = 2; // Coordinata y del rettangolo
+            float width = 333; // Larghezza del rettangolo
+            float height = 38; // Altezza del rettangolo
+            
+            stripper.addRegion("region", new java.awt.Rectangle((int) x, (int) y, (int) width, (int) height));
+            
+            for (int pageNumber = 1; pageNumber <= document.getNumberOfPages(); pageNumber++) {
+                stripper.extractRegions(document.getPage(pageNumber));
+            }
+            
+            document.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+
+        return dateextract;  
+        
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static void  recuperadati (String pathcsv,String datiestratti) throws IOException{
+
+        // prende il contenuto del file e lo mette in una stringa
+        String fileContent = Files.readString(Path.of(pathcsv));
+        System.out.println("il file contine: " +fileContent);
+        
+        // prendo anno e mese da nome file
+        String annomese = fileContent.substring(14, 20);
+        System.out.println("anno e mese: " +annomese);
+        
+        // metodo che estrae la categoria
+        String categoria = categoria(datiestratti);
+        System.out.println("Categoria del documento: " +categoria);
+
+
+
+        
+        //procedo con la creazione del file csv
+        createcsv();
+    }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static void createcsv(){
+
+    }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static String categoria(String datiestratti){
+        
+        String titolo = "";
+        String Nreport = "";
         String regex1 = "Titolo suppl\\.\\s+(.+)";
         String regex2 = "(N\\. report *)(\\d+)";
         
         Pattern pattern = Pattern.compile(regex1);
-        Matcher match = pattern.matcher(dateextract);
-        System.out.println(dateextract);
+        Matcher match = pattern.matcher(datiestratti);
+        System.out.println(datiestratti);
       
         if (match.find()) {
             String match1 = match.group(1);
             
             titolo = match1;
+           // titolo = titolo.replace(" ", "");
             System.out.println("Testo dopo Titolo suppl.: " + match1);
         }
 
         Pattern pattern2 = Pattern.compile(regex2);
-        Matcher matcher2 = pattern2.matcher(dateextract);
+        Matcher matcher2 = pattern2.matcher(datiestratti);
         boolean found = false;
 
         while (matcher2.find()) {
@@ -58,9 +119,9 @@ public class Process {
         if (!found) {
             System.out.println("Nessun match per la regex N. report");
         }
-        dateextract = Nreport +" " +titolo;
-        return dateextract;  
-        
+        datiestratti = ";" +Nreport +" - " +titolo;
+
+        return datiestratti;
     }
 
 }
